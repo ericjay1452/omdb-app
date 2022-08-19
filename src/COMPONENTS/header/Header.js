8036ab6b789d9a312c9b06809511a216a17af79a
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-08-19 21:25:04 +0100

    Started working on my header

--- src/COMPONENTS/header/Header.js
+++ src/COMPONENTS/header/Header.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import User from "../../IMAGES/User.svg"
+import { Link } from 'react-router-dom'
+import "./Header.scss"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='relative block w-full'>
+      <nav className='logo-header'>
+          <ul className='nav_list'>
+            <li>
+            <Link to="/">
+            Movie App
+              </Link>
+            </li>
+
+            <li className='bg-neutral-900'>
+           <Link to="/">
+           <img src= {User} alt = "user" className='max-w-full'/>
+           </Link> 
+            </li>
+
+          </ul>
+            
+      </nav>
+    </header>
   )
 }
 
