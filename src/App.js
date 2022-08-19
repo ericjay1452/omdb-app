b25af3b1a937d2515828f0b9f87a18ac4843ef8c
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-08-19 07:36:38 +0100

    Started project

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+function App() {
+  return (
+    <div className="App bg-slate-900">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    
+    </div>
+  );
+}
+
+export default App;
