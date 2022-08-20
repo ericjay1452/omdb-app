b27a22f2595dfdf2f3a75f4c175bfea263231037
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-08-20 20:29:07 +0100

    resumed back to work

--- src/COMPONENTS/home/Home.js
+++ src/COMPONENTS/home/Home.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import MoviesArray from '../moviesArray/MoviesArray'
-
+import {MovieApi} from "../../UTILS/api/MovieApi"
+import {Apikey} from "../../UTILS/api/Apikey"
 const Home = () => {
+
+  useEffect( () =>{
+   const ApiFetched = async () => {
+    const fetchedData = await MovieApi.get(`?apikey=${Apikey}&s=Harry`)
+   }
+  }, [])
   return (
     <div>
       <div className = "banner-image"></div>
