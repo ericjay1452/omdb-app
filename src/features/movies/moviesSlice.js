2772c1c290dab3a0ed443341631b622c50630318
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-08-20 20:57:12 +0100

    Started working on Redux toolkit

---
+++ src/features/movies/moviesSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    movies : [],
+}
+
+const moviesSlice = createSlice({
+    name : "movies",
+    initialState,
+    reducers : {
+
+    }
+})
