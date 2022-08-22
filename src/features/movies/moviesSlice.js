40039fb8242e918cec6e6d0f7da443cd8d039f50
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-08-22 02:30:53 +0100

    Implemented createAsynThunks for data fetching

--- src/features/movies/moviesSlice.js
+++ src/features/movies/moviesSlice.js
@@ -1,4 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import MovieApi from "../../UTILS/api/MovieApi"
+import {Api_key} from "../../UTILS/api/Apikey"
+import { useDispatch } from 'react-redux'
+
+
+const fetchedMoviesAsync = ("movies/fetchedApi", async () => {
+        const fetchedData = await MovieApi.get
+        (`?apikey=${Api_key}&s=Harry&type=movie`).catch(
+          (err) => {
+            console.log(`Err  :  ${err}`)
+          }
+        )
+         return fetchedData.data
+})
 
 const initialState = {
     movies : {},
